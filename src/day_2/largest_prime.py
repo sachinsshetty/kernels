14c87796060aab64dda498818d0603945f502df1
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-09-18 13:45:21 +0200

    fiond-largest

---
+++ src/day_2/largest_prime.py
@@ -0,0 +1,15 @@
+print("finding Largest Prime")
+
+max_value = 100 
+
+temp_array = []
+
+count = 1
+
+for value in range(1, max_value,2):
+    temp_array.append(value)
+
+print(len(temp_array))
+
+for value in temp_array:
+    print(value)
